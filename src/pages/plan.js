220e6c582b6d40aeffef97f74dbcd8aef8e0322c
jNembhard <nembhardjl@outlook.com>
2021-11-10 15:49:44 -0500

    options path fix

--- src/pages/plan.js
+++ src/pages/plan.js
@@ -3,7 +3,7 @@ import Layout from "../components/organisms/Layout"
 import Seo from "../components/seo"
 import PlanHero from "../components/molecules/PlanHero"
 import ProcessPlan from "../components/organisms/ProcessPlan"
-import Options from "../components/Organisms/Options"
+import Options from "../components/organisms/Options"
 
 const PlanPage = () => (
   <Layout>
